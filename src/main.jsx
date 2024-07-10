d7a0402b3d8f286d29aaffdebe4d21f83c1d7f93
“Eslam“ <aslamalmswdya@gmail.com>
2024-07-10 20:44:20 +0300

    fix errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+ 
 import './index.css'
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Category from "./components/Category";
+import Items from "./components/Items";
+import {items} from "./data";
+import { useState } from "react";
+import Footer from "./components/Footer";
+ 
+const [itemsData , setItemsData] =useState(items);
 
+const allCategory = ["الكل" , ...new Set(items.map((i)=>i.category))];
+
+const  filterByCategory = (cat)=>{
+  if(cat === "الكل"){
+    setItemsData(items)
+  }else{
+  const   newArr = items.filter((item => item.category === cat))
+  setItemsData(newArr)
+  }
+} 
+
+const  filterBySearch = (word)=>{
+  if(word != ""){
+  const newArr = items.filter((item => item.title === word))
+  setItemsData(newArr)
+  }
+} 
+
+ 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+  <div className="">
+  <NavBar filterBySearch={filterBySearch} />
+  <Container>
+    <Header />
+    <Category filterByCategory={filterByCategory} allCategory={allCategory}/>
+    <Items itemsData={itemsData} />
+   
+
+  </Container>
+  <Footer/>
+</div>
+ 
 )
+
+
+
+
+
